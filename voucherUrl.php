d946897be9d9e39b7cb6d7377911da2ba255bd1d
Andrea Bergamasco <a.bergamasco@groupecomplus.com>
2023-08-31 10:32:19 +0200

    [Refactoring][WIP] Replaced Discount with CartRule.
    See TODOs for the other deprecations pending a fix.
    The promo code is not added to the cart yet.

--- voucherUrl.php
+++ voucherUrl.php
@@ -1,8 +1,15 @@
 <?php
 
-class voucherUrl extends Module{
-    
-	public function __construct(){
+require_once (__DIR__ . '/../../classes/Cart.php');
+
+use CartCore as Cart;
+
+class voucherUrl extends Module
+{
+    private const DEBUG_MODE = true;
+
+    public function __construct()
+    {
         $this->name = 'voucherUrl';
         $this->tab = 'advertising_marketing';
         $this->version = 0.1;
@@ -11,58 +18,80 @@ class voucherUrl extends Module{
         parent::__construct();
 
         /* Nombre y descripcion que se muestra en la seccion de modulos */
-
         $this->displayName = $this->l('Vales Descuento en Url');
         $this->description = $this->l('Permite añadir vales descuento por parametro (\'voucher\') en cualquier lugar');
     }
-	
-    public function install(){
-        if (!parent::install() OR
-            !$this->registerHook('header') OR
+
+    public function install(): bool
+    {
+        if (!parent::install() or
+            !$this->registerHook('header') or
             !$this->registerHook('cart')
-        )
+        ) {
             return false;
+        }
+
         return true;
     }
 
-    public function unistall(){
-	
-        return parent::unistall();
+    public function uninstall()
+    {
+
+        return parent::uninstall();
     }
 
-    public function hookHeader(){
+    public function hookHeader()
+    {
         global $cookie;
-        if (Tools::getValue('voucher')){
+
+        if (Tools::getValue('voucher')) {
             $cookie->voucherCode = Tools::getValue('voucher');
         }
     }
 
-    public function hookCart($params){
+    public function hookCart($params)
+    {
+        if (!isset($params['cookie']->voucherCode)) {
+            self::DEBUG_MODE && error_log('voucherCode not found in cookie');
+            return;
+        }
 
-        if (isset($params['cookie']->voucherCode )){
+        self::DEBUG_MODE && error_log('voucherCode found in cookie');
 
-            $vouchersInCart = $params['cart']->getDiscounts();
-            $vouchersInCartIds = array();
-            if (count($vouchersInCart)>0){
-                foreach($vouchersInCart as $voucher){
-                    $vouchersInCartIds[] = $voucher['id_discount'];
-                }
-            }
-            $idDiscount = Discount::getIdByName($params['cookie']->voucherCode);
+        /** @var Cart $cart */
+        $cart = $params['cart'];
 
-            if (in_array($idDiscount,$vouchersInCartIds)){
-                unset($params['cookie']->voucherCode);
-            }
-            else{
-                $discountObj = New Discount($idDiscount);
-                $totalType = Cart::ONLY_PRODUCTS_WITHOUT_SHIPPING;
-                if ($params['cart']->getOrderTotal(true,$totalType) >= Configuration::get(PS_PURCHASE_MINIMUM) && $params['cart']->getOrderTotal(true,$totalType) >= $discountObj->minimal){
-                        $params['cart']->addDiscount($idDiscount);
-                        unset($params['cookie']->voucherCode);
-                }
-            }
+        $vouchersInCartIds = array_map(function (array $voucher) {
+            return $voucher['id_discount'];
+        }, $cart->getDiscounts());
+
+        self::DEBUG_MODE && error_log('discount IDs found in current cart : ' . print_r($vouchersInCartIds, true));
+
+        $idDiscount = CartRule::getIdByCode($params['cookie']->voucherCode);
+
+        if (in_array($idDiscount, $vouchersInCartIds)) {
+            unset($params['cookie']->voucherCode);
+            return;
+        }
+        $discountObj = new CartRule($idDiscount);
+        $totalType = Cart::ONLY_PRODUCTS_WITHOUT_SHIPPING; // TODO : update deprecated const
+
+        if (
+            ($cart->getOrderTotal(true, $totalType) >= Configuration::get(PS_PURCHASE_MINIMUM)) // TODO: PHP Warning:  Use of undefined constant PS_PURCHASE_MINIMUM
+            && ($cart->getOrderTotal(true, $totalType) >= $discountObj->minimum_amount) // TODO: update
+        ) {
+            self::DEBUG_MODE && error_log("add dìscount {$idDiscount} to cart, remove voucherCode from cookie");
+
+            $cart->addCartRule($idDiscount);
+
+            self::DEBUG_MODE && error_log('cart discounts updated : ' . print_r($cart->getDiscounts(), true));
+
+            unset($params['cookie']->voucherCode);
         }
     }
-}
 
-?>
+    public function isUsingNewTranslationSystem(): bool
+    {
+        return true;
+    }
+}
